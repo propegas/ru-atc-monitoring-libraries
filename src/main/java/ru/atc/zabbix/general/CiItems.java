1b6ef085a5d88dfafc0205a13bc8fb5719338a74
vgoryachev <vgoryachev@at-consulting.ru>
2018-04-26 17:10:52 +0300

    update for web items CI

--- src/main/java/ru/atc/zabbix/general/CiItems.java
+++ src/main/java/ru/atc/zabbix/general/CiItems.java
@@ -8,12 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static ru.atc.zabbix.general.HashGenerator.hashString;
-
-//import static ru.atc.camel.zabbix.general.HashGenerator.hashString;
-
-//import ru.atc.camel.zabbix.devices.ZabbixAPIConsumer;
-
 /**
  * Created by vgoryachev on 01.03.2016 ${PACKAGE_NAME}.
  * Package: ${PACKAGE_NAME}.
@@ -21,15 +15,21 @@ import static ru.atc.zabbix.general.HashGenerator.hashString;
 public class CiItems {
     private static Logger logger = LoggerFactory.getLogger(CiItems.class);
 
+    private CiItems() {
+    }
+
     public static String checkHostPattern(String hostHost, String hostName) {
+        return checkHostPattern(hostHost, hostName, "(.*)--(.*)");
+    }
+
+    public static String checkHostPattern(String hostHost, String hostName, String hostAliasPattern) {
         String ciHostAliasName = null;
         // Example: KRL-PHOBOSAU--MSSQL
-        //String[] hostreturn = new String[] { "", "" } ;
-        Pattern p = Pattern.compile("(.*)--(.*)");
+        Pattern p = Pattern.compile(hostAliasPattern);
 
         Matcher hostHostMatcher = p.matcher(hostHost.toUpperCase());
         Matcher hostNameMatcher = p.matcher(hostName.toUpperCase());
-        //String output = "";
+
         if (hostHostMatcher.matches()) {
             logger.debug("*** Use hostHost for alias name: " + hostHost.toUpperCase());
             ciHostAliasName = hostHost;
@@ -41,13 +41,13 @@ public class CiItems {
     }
 
     public static String[] checkItemForCi(String itemname, String hostid, String hostname,
-                                          String itemCiPattern, String itemCiParentPattern, String itemCiTypePattern) {
-
-        //logger.debug("*** Received Zabbix Item : " + itemname);
+                                          String itemCiPattern, String itemCiParentPattern,
+                                          String itemCiTypePattern) throws Exception {
 
         // Example item as CI :
         // [test CI item] bla-bla
         // [CI 2 (TYPE)::CI 3 (TYPE)] trapper item status
+        String checkItemName = itemname;
 
         // zabbix_item_ke_pattern=\\[(.*)\\](.*)
         Pattern itemWithCiPattern = Pattern.compile(itemCiPattern);
@@ -58,25 +58,39 @@ public class CiItems {
         // zabbix_item_ci_type_pattern=(.*)\\((.*)\\)
         Pattern ciWithTypePattern = Pattern.compile(itemCiTypePattern);
 
-        Matcher matcher = itemWithCiPattern.matcher(itemname);
         String ciId;
-        String newItemName = "";
+        String newItemName;
         String ciName;
         String deviceType = "";
-        String parentItemName;
+        String parentItemName = "";
         String parentCiId = "";
-        //String hostnameend = "";
 
+        // WEB items
+        Pattern p = Pattern.compile(".*((of|for) scenario)\\s\"(\\[(.*)](.*))\"\\.?");
+        Matcher webMatcher = p.matcher(checkItemName);
+        logger.debug("*** Check WEB test item: {}", checkItemName);
+
+        if (webMatcher.matches()) {
+            logger.debug("*** Found WEB test item: {}", checkItemName);
+            String group = webMatcher.group(3);
+
+            if (itemWithCiPattern.matcher(group).matches()) {
+                logger.debug("*** Found item '{}' with CI Pattern in WEB test", group);
+                checkItemName = group;
+            }
+
+        }
+
+        Matcher matcher = itemWithCiPattern.matcher(checkItemName);
         // if Item has CI pattern
         // [CI 2 (TYPE)::CI 3 (TYPE)] trapper item status
         logger.debug("*** Using CI pattern: " + itemWithCiPattern);
         if (matcher.matches()) {
 
-            logger.debug("*** Finded Zabbix Item with Pattern as CI: " + itemname);
+            logger.debug("*** Finded Zabbix Item with Pattern as CI: " + checkItemName);
 
             // save as CI name
             newItemName = matcher.group(1).toUpperCase();
-
             ciName = newItemName;
 
             logger.debug("*** newitemname: " + newItemName);
@@ -85,8 +99,9 @@ public class CiItems {
             Matcher matcher2 = ciWithParentPattern.matcher(newItemName);
             if (matcher2.matches()) {
                 logger.debug("*** Finded Zabbix Item with Pattern with Parent: " + newItemName);
-                newItemName = matcher2.group(2).trim().toUpperCase();
 
+                // get name from ::parent
+                newItemName = matcher2.group(2).trim().toUpperCase();
                 ciName = newItemName;
 
                 parentItemName = matcher2.group(1).trim().toUpperCase();
@@ -95,13 +110,14 @@ public class CiItems {
 
                 logger.debug(String.format("*** Trying to generate hash for ParentItem with Pattern: %s:%s",
                         hostname, parentItemName));
-                String hash = "";
-                try {
-                    hash = hashString(String.format("%s:%s", hostname, parentItemName), "SHA-1");
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                String hash;
+                //if ("".equals(parentItem))
+                //hash = hashString(String.format("%s:%s", hostname, parentItemName), "SHA-1");
+                hash = String.format("%s:%s", hostname, parentItemName);
+               /* else
+                    hash = hashString(String.format("%s:%s:%s", hostname, parentItem, parentItemName), "SHA-1");
+              */  //hash = hashString(String.format("%s:%s", hostname, parentItemName), "SHA-1");
+
                 logger.debug("*** Generated Hash: " + hash);
                 parentCiId = hash;
 
@@ -121,59 +137,81 @@ public class CiItems {
                 logger.error(String.format("Error while parsing CI type using regexp pattern: %s ", e));
             }
 
+            // ci 2 | ci.id
+            try {
+                Pattern ciWithCiIdPattern = Pattern.compile("(.*) \\| (.*)");
+                Matcher matcher4 = ciWithCiIdPattern.matcher(newItemName);
+                if (matcher4.matches()) {
+                    logger.debug("*** Finded Zabbix Item with Pattern with CI ID: " + newItemName);
+                    newItemName = matcher4.group(1).trim().toUpperCase();
+                    logger.debug("*** newitemname: " + newItemName);
+                }
+            } catch (Exception e) {
+                logger.error(String.format("Error while parsing CI type using regexp pattern: %s ", e));
+            }
+
             // get SHA-1 hash for hostname-item block for saving as ciid
             // Example:
             // KRL-PHOBOSAU--PHOBOS:TEST CI ITEM
-            logger.debug(String.format("*** Trying to generate hash for Item with Pattern: %s:%s",
-                    hostname, ciName));
-            String hash = "";
-            try {
-                hash = hashString(String.format("%s:%s", hostname, ciName), "SHA-1");
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            logger.debug(String.format("*** Trying to generate hash for Item with Pattern: %s:%s:%s",
+                    hostname, parentItemName, ciName));
+            String hash;
+            //if ("".equals(parentItem))
+            //hash = hashString(String.format("%s:%s", hostname, ciName), "SHA-1");
+            hash = String.format("%s:%s", hostname, ciName);
+           /* else
+                hash = hashString(String.format("%s:%s:%s", hostname, parentItem, ciName), "SHA-1");
+*/
             logger.debug("*** Generated Hash: " + hash);
             ciId = hash;
 
-            //event.setParametr(itemname);
         }
         // if Item has no CI pattern
         else {
-            logger.debug("*** No matches to CI pattern, use parent host as CI for item: " + itemname);
+            logger.debug("*** No matches to CI pattern, use parent host as CI for item: " + checkItemName);
             ciId = hostid;
-            newItemName = itemname;
+            newItemName = checkItemName;
         }
 
         // id, name, type, parentid
-        String[] hostreturn = new String[]{"", "", "", ""};
+        String[] hostreturn = new String[]{"", "", "", "", ""};
         hostreturn[0] = ciId;
         hostreturn[1] = newItemName;
         hostreturn[2] = deviceType;
         hostreturn[3] = parentCiId;
-        //hostreturn[1] = hostnameend;
+        hostreturn[4] = parentItemName;
 
         logger.debug("New Zabbix CI ID: " + hostreturn[0]);
         logger.debug("New Zabbix CI Name: " + hostreturn[1]);
         logger.debug("New Zabbix DeviceType: " + hostreturn[2]);
         logger.debug("New Zabbix ParentID: " + hostreturn[3]);
+        logger.debug("New Zabbix Parent CI Name: " + hostreturn[4]);
 
         return hostreturn;
     }
 
-    public static String[] checkHostAliases(JSONArray allZabbixHosts, String hostHost, String hostName) {
+    public static String[] checkHostAliases(JSONArray allZabbixHosts, String hostHost,
+                                            String hostName) {
+        return checkHostAliases(allZabbixHosts, hostHost, hostName, "(.*)--(.*)");
+    }
+
+    public static String[] checkHostAliases(JSONArray allZabbixHosts, String hostHost,
+                                            String hostName, String hostAliasPattern) {
 
         // Example: KRL-PHOBOSAU--MSSQL
         String[] hostreturn = new String[]{"", "", ""};
-        Pattern p = Pattern.compile("(.*)--(.*)");
+        Pattern p = Pattern.compile(hostAliasPattern);
 
         logger.debug("*** Check hostHost or hostName for aliases: " + hostHost.toUpperCase());
 
         String hostnameBeginPart = "";
         String hostnameEndPart = "";
-        //String output = "";
 
-        String host = CiItems.checkHostPattern(hostHost, hostName);
+        String host;
+        host = CiItems.checkHostPattern(hostHost, hostName, hostAliasPattern);
+        if (host == null)
+            return hostreturn;
+
         Matcher hostMatcher = p.matcher(host.toUpperCase());
         if (hostMatcher.matches()) {
             hostnameEndPart = hostMatcher.group(2).toUpperCase();
@@ -182,35 +220,29 @@ public class CiItems {
             logger.debug("*** hostnameEndPart: " + hostnameEndPart);
         }
 
-        //else return
-        //hostgroupsloop:
-        String ParentID = "";
+        String parentID = "";
 
-        if (allZabbixHosts != null && 0 != allZabbixHosts.size()) {
+        if (allZabbixHosts != null && !allZabbixHosts.isEmpty()) {
             int j = 0;
             while (j < allZabbixHosts.size()) {
-                // logger.debug(f.toString());
-                // ZabbixAPIHost host = new ZabbixAPIHost();
-                JSONObject host_a = allZabbixHosts.getJSONObject(j);
-                String compareHost = host_a.getString("host");
+
+                JSONObject comparingZabbixHost = allZabbixHosts.getJSONObject(j);
+
+                String compareHost = comparingZabbixHost.getString("host");
+                String compareName = comparingZabbixHost.getString("name");
                 logger.debug("*** Compare with hosthost: " + compareHost);
-                if (compareHost.equalsIgnoreCase(hostnameBeginPart)) {
-                    ParentID = host_a.getString("hostid");
-                    break;
-                }
-
-                String compareName = host_a.getString("name");
                 logger.debug("*** Compare with hostname: " + compareName);
-                if (compareName.equalsIgnoreCase(hostnameBeginPart)) {
-                    ParentID = host_a.getString("hostid");
+                if (compareHost.equalsIgnoreCase(hostnameBeginPart) ||
+                        compareName.equalsIgnoreCase(hostnameBeginPart)) {
+                    parentID = comparingZabbixHost.getString("hostid");
                     break;
-
                 }
+
                 j++;
             }
         }
 
-        hostreturn[0] = ParentID;
+        hostreturn[0] = parentID;
         hostreturn[1] = hostnameEndPart;
         hostreturn[2] = hostnameBeginPart;
 
@@ -220,59 +252,55 @@ public class CiItems {
         return hostreturn;
     }
 
-    public static String getTransformedItemName(String name, String key) {
+    public static String getTransformedItemName(String itemName, String key) {
 
         // get params from key to item $1 placeholder
         // Example:
         // vfs.fs.size[/oracle,pfree]
 
-        Pattern p = Pattern.compile("(.*)\\[(.*)\\]");
+//        Pattern p = Pattern.compile("(.*)\\[(.*)\\]");
+        Pattern p = Pattern.compile("([0-9a-zA-Z_\\-.]*)\\[(.*)\\]");
         Matcher matcher = p.matcher(key);
 
-        String keyparams;
-        //String webscenario = "";
-        //String webstep = "";
-
+        String itemKeyParams;
         String[] params = new String[]{};
 
         if (matcher.matches()) {
 
             logger.debug("*** Finded Zabbix Item key with $1-$9 placeholders Pattern: " + key);
             // vfs.fs.size[/oracle,pfree]
-            keyparams = matcher.group(2);
+            itemKeyParams = matcher.group(2);
 
             // [/oracle,pfree]
-            params = keyparams.split(",");
+            params = itemKeyParams.split(",");
             logger.debug(String.format("*** Finded Zabbix Item key params (size): %d ", params.length));
-
 
         }
         // if Item has no $1 placeholders pattern
         else {
-
             logger.debug("*** Zabbix $1-$9 placeholders Pattern not found fro key: " + key);
         }
 
-        logger.debug("Item name: " + name);
+        logger.debug("Item name: " + itemName);
+        String nameWithReplacedPlaceholders = itemName;
 
         String param;
-        int paramnumber;
-        Matcher m = Pattern.compile("\\$\\d+").matcher(name);
+        int paramIndex;
+        Matcher m = Pattern.compile("\\$\\d+").matcher(itemName);
         while (m.find()) {
             param = m.group(0);
-            paramnumber = Integer.parseInt(param.substring(1));
-            logger.debug("Found Param: " + paramnumber);
+            paramIndex = Integer.parseInt(param.substring(1));
+            logger.debug("Found Param: " + paramIndex);
             logger.debug("Found Param Value: " + param);
-            logger.debug("Found Param Value Replace: " + params[paramnumber - 1]);
+            logger.debug("Found Param Value Replace: " + params[paramIndex - 1]);
 
-            name = name.replaceAll("\\$" + paramnumber, params[paramnumber - 1]);
+            nameWithReplacedPlaceholders = itemName.replaceAll("\\$" + paramIndex, params[paramIndex - 1]);
 
         }
 
+        logger.debug("New Zabbix Item Name: " + nameWithReplacedPlaceholders);
 
-        logger.debug("New Zabbix Item Name: " + name);
-
-        return name;
+        return nameWithReplacedPlaceholders;
 
     }
 }
